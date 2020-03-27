919390acb3a5fa339ad624d7ff2fd5a31787c22c
argush3 <argus@highwaythreesolutions.com>
2020-03-27 06:51:19 -0700

    Refactor to have reusable alert components

---
+++ frontend/src/components/AlertError.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-alert style="margin-left: 10px; margin-bottom: 12px; width:65%;"
+             text
+             prominent
+             type="error"
+             icon="mdi-cloud-alert">{{message}}</v-alert>
+</template>
+
+<script>
+
+    export default {
+        props: {
+            message: {
+                type: String,
+                required: true,
+                default: () => ''
+            },
+        },
+        data() {
+            return {
+            }
+        },
+    };
+</script>
+
+<style scoped>
+</style>
