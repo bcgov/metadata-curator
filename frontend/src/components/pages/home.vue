dd5cfcf7194c33f5b7129d95c4ca230c1ba091a2
argush3 <argus@highwaythreesolutions.com>
2020-03-29 07:30:34 -0700

    Clean up endpoints

--- frontend/src/components/pages/home.vue
+++ frontend/src/components/pages/home.vue
@@ -24,12 +24,6 @@ export default {
             resetState: 'file/resetState'
         }),
     },
-    data () {
-        return {
-            tab: null,
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        }
-    },
 }
 </script>
 <style scoped>
