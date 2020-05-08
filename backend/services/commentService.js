637dcb82cb21f4408d4dd08ac7ab44cce28bb687
argush3 <argus@highwaythreesolutions.com>
2020-05-08 10:27:36 -0700

    Start refactoring backend to make more testable

---
+++ backend/services/commentService.js
@@ -0,0 +1,15 @@
+const db = require('../db/db');
+
+const addComment = async () => {
+    try {
+
+    } catch(e) {
+        throw new Error(e.message)
+    }
+}
+
+module.exports = {
+    addComment
+}
+
+
