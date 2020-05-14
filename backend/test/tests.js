1aa00cbc86a8dd7d7989ae0c88d73832e6c17d0a
ikethecoder <ikethecoder@canzea.com>
2020-05-13 17:28:20 -0700

    unit tests for comments and dataupload

--- backend/test/tests.js
+++ backend/test/tests.js
@@ -2,4 +2,6 @@ process.env.NODE_ENV = 'test';
 
 describe("MC Unit Tests", function() {
     require('./v1/dataUploads');
+    require('./services/dataUploadServiceTest');
+    require('./services/commentServiceTest');
 });
