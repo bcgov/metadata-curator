0b7a75c8517c5d7ee0d5e9d192ae34b39d01e47f
BrandonSharratt <brandon@highwaythreesolutions.com>
2020-05-15 14:34:25 -0700

    Greatly improved encrypt/upload time after initial read by making it more asynchronous

--- frontend/src/components/FileReader.vue
+++ frontend/src/components/FileReader.vue
@@ -3,6 +3,32 @@
         <v-file-input v-model="file" :disabled="disabled" counter show-size label="File input" style="margin-top:0px;padding-top:0px;"></v-file-input>
         <v-btn v-if="showUploadButton" :disabled="disabled" @click="upload">Upload</v-btn>
         <v-btn v-if="showImportButton" :disabled="disabled" @click="onImportButtonClicked">Import</v-btn>
+        <div v-if="pleaseWait">
+            <v-progress-circular
+                indeterminate
+            ></v-progress-circular>
+            <span>Please wait while the first bit of the file is encrypted...</span>
+        </div>
+        <div v-if="showProgress">
+            <span>
+                {{progressMessage1}}
+            </span>
+            <br />
+            <span>
+                {{progressMessage2}}
+            </span> 
+            <br />
+            <span>
+                {{progressMessage3}}
+                <v-progress-linear :value="(up1Progress/up1Size)*100"></v-progress-linear>
+            </span>
+            <br />
+            <span>
+                {{progressMessage4}}
+                <v-progress-linear :value="(up2Progress/up2Size)*100"></v-progress-linear>
+            </span>
+            <br />
+        </div>
     </span>
 </template>
 
@@ -47,6 +73,15 @@ export default {
             numChunks: 0,
             uploads: [],
             currChunk: 0,
+            numEncrypted: 0,
+
+            numUploaded: 0,
+            up1Size: 0,
+            up1Progress: 0,
+            up2Size: 0,
+            up2Progress: 0,
+            showProgress: false,
+            pleaseWait: false,
         }
     },
 
@@ -59,30 +94,48 @@ export default {
         }),
         ...mapGetters({
             getStringContent: 'file/getStringContent'
-        })
+        }),
+
+        progressMessage1: function(){
+            return `Encrypted: ${this.numEncrypted}/${this.numChunks}` 
+        },
+        progressMessage2: function(){
+            return `Uploaded: ${this.numUploaded}/${this.numChunks}` 
+        },
+        progressMessage3: function(){
+            return `Upload 1: ${this.up1Progress}/${this.up1Size}` 
+        },
+        progressMessage4: function(){
+            return `Upload 2: ${this.up2Progress}/${this.up2Size}` 
+        }
 
     },
 
     watch: {
-        async file(){
+        file(){
             this.uploads = [];
             this.numChunks = 0;
             this.currChunk = 0;
             
             if ( (this.readFile) && (this.file) ){    
                 this.disabled = true;
-                await this.getNextChunk();
-                this.disabled = false;
+                var self = this;
+                this.pleaseWait = true;
+                this.getNextChunk(0).then( () => {
+                    self.numEncrypted += 1
+                    self.disabled = false;
+                    self.pleaseWait = false;
+                });
 
             }else if ((!this.file) && (this.mutateVuex)){
-                this.$store.commit('file/setContent', { content: new Uint8Array() } );
+                this.$store.commit('file/clearContent');
             }
         }
     },
 
     methods: {
 
-        getNextChunk: function(){
+        getNextChunk: async function(index){
             return new Promise((resolve, reject) => {
 
                 var reader = new FileReader();
@@ -94,18 +147,21 @@ export default {
                 reader.onload = async function(e){
                     if (self.mutateVuex === true){
                         // console.log("watch filename: " + self.file.name);
+                        
+                        let content = new Uint8Array(e.target.result);
+
                         if (self.offset === 0){
-                            await self.$store.commit('file/setContent', { content: new Uint8Array(e.target.result)})
                             self.$store.commit('file/setFileName', { fileName: self.file.name})
-                            await self.$store.dispatch('file/encryptContent');
-                            resolve(e.target.result);
-                        }else{
-                            await self.$store.commit('file/setUploadContent', { content: new Uint8Array(e.target.result)})
-                            await self.$store.dispatch('file/encryptUploadContent');
-                            resolve(e.target.result);
+                            await self.$store.commit('file/setContent', { content: content, index: index})
                         }
+
+                        self.$store.dispatch('file/encryptContent', {clear: self.offset === 0, index: index, content: content}).then( () => {
+                            resolve(e.target.result);
+                        });
+                        
                     }else{
                         self.fileContent = e.target.result;
+                        resolve(e.target.result)
                     }
                     //see below comment for why this is floor instead of ceil
                     self.numChunks = Math.floor(self.file.size / self.chunkSize);
@@ -128,7 +184,9 @@ export default {
 
         upload: function(){
             this.disabled = true;
+            this.showProgress = true;
             var i = 0;
+            var self = this;
             var tus = require("tus-js-client");
             var fing = function(file, options, callback){
                 return callback(null, [
@@ -161,44 +219,75 @@ export default {
                 },
                 onProgress: (bytesUploaded, bytesTotal) => {
                     // eslint-disable-next-line
-                    console.log("Upload progress " + bytesUploaded + "/" + bytesTotal);
+                    this.up1Progress = bytesUploaded;
+                    this.up1Size = bytesTotal;
                 },
                 onSuccess: async() => {
                     i += 1;
+                    this.numUploaded += 1;
 
-                    if (i<=this.numChunks){
-                        await this.getNextChunk();
-                    }
-
-                    if ((i<=this.numChunks) && (this.blob[0].size > 0)){
-                        let u2 = new tus.Upload(this.blob[0], uploadOptions);
-                        u2.start();
-                        this.uploads.push(u2);
-                    }else if ( (i !== 1) && (i >= this.numChunks) ){
-                        let joinIds = [];
-                        for (let j=0; j<this.uploads.length; j++){
-                            let url = this.uploads[j].url.substring(this.uploads[j].url.substring(9).indexOf("/")+9);
-                            joinIds.push(url);
-                        }
-                        backendApi.concatenateUpload(joinIds, this.uploadUrl, this.jwt, "1.0.0").then( () => {
-                            this.disabled = false;
-                        }).catch( (/*e*/) => {
-                            // eslint-disable-next-line
-                            console.log("Concatenation error", error);
-                            this.disabled = false;
+                    if ( (i+1) <= this.numChunks){
+                        let chunkIndex = 2;
+                        this.getNextChunk(chunkIndex).then( () => {
+                            self.numEncrypted += 1
+                            if ((i<self.numChunks) && (self.blob[chunkIndex].size > 0)){
+                                uploadOptions.onProgress = function(byteUp, byteTot){
+                                    self.up2Progress = byteUp;
+                                    self.up2Size = byteTot;
+                                }
+                                let u2 = new tus.Upload(self.blob[chunkIndex], uploadOptions);
+                                u2.start();
+                                self.uploads.push(u2);
+                            }else if ( (i !== 1) && (i >= self.numChunks) ){
+                                let joinIds = [];
+                                for (let j=0; j<self.uploads.length; j++){
+                                    let url = self.uploads[j].url.substring(self.uploads[j].url.substring(9).indexOf("/")+9);
+                                    joinIds.push(url);
+                                }
+                                backendApi.concatenateUpload(joinIds, self.uploadUrl, self.jwt, "1.0.0").then( () => {
+                                    self.numUploaded += 1;
+                                    self.disabled = false;
+                                }).catch( (/*e*/) => {
+                                    // eslint-disable-next-line
+                                    console.log("Concatenation error", error);
+                                    self.disabled = false;
+                                });
+                            }
                         });
-                        
-                        //let u2 = new tus.Upload(this.blob[0], uploadOptions);
-                        //u2.start();
-                        
-                    }
+                    }                        
                 },
             }
-            if (this.blob.length === 1){
+
+            if (this.blob[0].size <= this.chunkSize){
                 uploadOptions.headers = {};
             }
+
             let u = new tus.Upload(this.blob[0], uploadOptions);
             this.uploads.push(u);
+            this.getNextChunk(1).then( () => {
+                self.numEncrypted += 1
+                let chunkIndex = 1;
+                i += 1;
+                if ((i<self.numChunks) && (self.blob[chunkIndex].size > 0)){
+                    let u2 = new tus.Upload(self.blob[chunkIndex], uploadOptions);
+                    u2.start();
+                    self.uploads.push(u2);
+                }else if ( (i !== 1) && (i >= self.numChunks) ){
+                    let joinIds = [];
+                    for (let j=0; j<self.uploads.length; j++){
+                        let url = self.uploads[j].url.substring(self.uploads[j].url.substring(9).indexOf("/")+9);
+                        joinIds.push(url);
+                    }
+                    backendApi.concatenateUpload(joinIds, self.uploadUrl, self.jwt, "1.0.0").then( () => {
+                        self.numUploaded += 1;
+                        self.disabled = false;
+                    }).catch( (/*e*/) => {
+                        // eslint-disable-next-line
+                        console.log("Concatenation error", error);
+                        self.disabled = false;
+                    });
+                }
+            });
             u.start();
         },
         onImportButtonClicked: function(){
