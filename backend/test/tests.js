78d786b4b07b6ca36ebf8e550bfd4c7daa6e10ea
ikethecoder <ikethecoder@canzea.com>
2020-05-14 18:28:37 -0700

    improve coverage for unit tests

--- backend/test/tests.js
+++ backend/test/tests.js
@@ -1,6 +1,10 @@
 process.env.NODE_ENV = 'test';
 
 describe("MC Unit Tests", function() {
+    require('./v1/dataPackagesTest');
+    require('./v1/metadataRevisionsTest');
+    require('./v1/repositoriesTest');
+    require('./v1/tableSchemaTest');
     require('./v1/dataUploads');
     require('./services/dataUploadServiceTest');
     require('./services/commentServiceTest');
