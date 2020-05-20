edd2f0f1e8721a87ced4c639fddc28844d0bc09b
argush3 <argus@highwaythreesolutions.com>
2020-05-20 12:46:35 -0700

    Refactoring of data upload page to use stepper/wizard vuetify component

--- frontend/src/components/pages/upload.vue
+++ frontend/src/components/pages/upload.vue
@@ -1,64 +1,46 @@
 <template>
-
-
     <v-container>
-        <v-stepper v-model="e1">
+        <v-stepper v-model="step">
             <v-stepper-header>
-                <v-stepper-step :complete="e1 > 1" step="1">Upload Info</v-stepper-step>
+                <v-stepper-step :step="steps.step1UploadForm" :complete="step > steps.step1UploadForm" >Upload Info</v-stepper-step>
                 <v-divider></v-divider>
-                <v-stepper-step :complete="e1 > 2" step="2">File Selection</v-stepper-step>
+                <v-stepper-step :step="steps.step2FileSelection" :complete="step > steps.step2FileSelection" >File Selection</v-stepper-step>
                 <v-divider></v-divider>
-                <v-stepper-step step="3" :complete="e1 > 3" >File Level Info</v-stepper-step>
+                <v-stepper-step :step="steps.step3FileLevelForm" :complete="step > steps.step3FileLevelForm" >File Level Info</v-stepper-step>
                 <v-divider></v-divider>
-                <v-stepper-step step="4" :complete="e1 > 4" >Upload Progress</v-stepper-step>
+                <v-stepper-step :step="steps.step4UploadProgress" :complete="step > steps.step4UploadProgress" >Upload Progress</v-stepper-step>
                 <v-divider></v-divider>
-                <v-stepper-step step="5">Upload Summary</v-stepper-step>
+                <v-stepper-step :step="steps.step5UploadSummary">Upload Summary</v-stepper-step>
             </v-stepper-header>
 
             <v-stepper-items>
-                <v-stepper-content step="1">
+                <v-stepper-content :step="steps.step1UploadForm">
                     <v-card class="mb-12">
-                        <UploadForm ref="uploadForm"></UploadForm>
+                        <UploadForm ref="uploadForm" :upload="upload"></UploadForm>
                     </v-card>
                     <v-btn color="primary" @click="stepSaveUploadForm">Next</v-btn>
-<!--                    <v-btn text>Cancel</v-btn>-->
                 </v-stepper-content>
 
-                <v-stepper-content step="2">
+                <v-stepper-content :step="steps.step2FileSelection">
                     <v-card class="mb-12"></v-card>
-                    <v-btn color="primary" @click="e1=3">Continue</v-btn>
-                    <v-btn text @click="e1=1">Back</v-btn>
+                    <v-btn color="primary" @click="step=steps.step3FileLevelForm">Next</v-btn>
+                    <v-btn text @click="step=steps.step1UploadForm">Back</v-btn>
                 </v-stepper-content>
 
-                <v-stepper-content step="3">
-                    <v-card
-                        class="mb-12"
-                    ></v-card>
-                    <v-btn
-                        color="primary"
-                        @click="e1 = 4"
-                    >
-                        Continue
-                    </v-btn>
+                <v-stepper-content :step="steps.step3FileLevelForm">
+                    <v-card class="mb-12"></v-card>
+                    <v-btn color="primary" @click="step=steps.step4UploadProgress">Next</v-btn>
+                    <v-btn text @click="step=steps.step2FileSelection">Back</v-btn>
+                </v-stepper-content>
 
-                    <v-btn text>Cancel</v-btn>
+                <v-stepper-content :step="steps.step4UploadProgress">
+                    <v-card class="mb-12"></v-card>
+                    <v-btn color="primary" @click="step=steps.step5UploadSummary">Next</v-btn>
+                    <v-btn text @click="step=steps.step3FileLevelForm">Back</v-btn>
                 </v-stepper-content>
-                <v-stepper-content step="4">
-                    <v-card
-                        class="mb-12"
-                    ></v-card>
-                    <v-btn
-                        color="primary"
-                        @click="e1 = 5"
-                    >
-                        Continue
-                    </v-btn>
-                    <v-btn text>Cancel</v-btn>
-                </v-stepper-content>
-                <v-stepper-content step="5">
-                    <v-card
-                        class="mb-12"
-                    ></v-card>
+
+                <v-stepper-content :step="steps.step5UploadSummary">
+                    <v-card class="mb-12"></v-card>
                 </v-stepper-content>
             </v-stepper-items>
         </v-stepper>
@@ -75,21 +57,42 @@
             UploadForm
         },
         created() {
-
+            if(this.$route.params.id && this.$route.params.id != 'new') { this.uploadId = this.$route.params.id; }
+            if(this.uploadId) {
+                // console.log("get upload ", this.uploadId);
+                this.getUpload(this.uploadId);
+            }
         },
         methods: {
             ...mapActions({
+                getUpload: 'upload/getUpload',
+                createInitialUpload: 'upload/createInitialUpload',
             }),
             ...mapMutations({
+                resetState: 'upload/resetState'
             }),
             triggerUploadFormSubmit() {
-                console.log("triggerUploadFormSubmit btn clicked");
                 this.$refs.uploadForm.submitForm();
             },
             async stepSaveUploadForm() {
               if(this.$refs.uploadForm.validateForm()) {
+                  if((!this.uploadId && !this.newUploadCreated) && !this.createUploadInProgress) {
+                      // console.log("create new upload");
+                      const submission = this.$refs.uploadForm.getSubmission();
+                      const initialUpload = {
+                          name: submission.data.datasetName,
+                          description: submission.data.datasetName,
+                          uploader: this.user.displayName
+                      }
+                      this.createInitialUpload(initialUpload);
+                      await this.triggerUploadFormSubmit();
+                  }
+                  else {
+                      // console.log("update existing upload submission");
+                      await this.triggerUploadFormSubmit();
+                  }
                   await this.triggerUploadFormSubmit();
-                  this.e1 = 2;
+                  this.step = this.steps.step2FileSelection;
               }
               else {
                 //still trigger submit so form displays all validation errors on UI
@@ -99,18 +102,54 @@
         },
         data () {
             return {
-                e1: 1,
+                uploadId: null,
+                step: 1,
+                steps: {
+                    step1UploadForm: 1,
+                    step2FileSelection: 2,
+                    step3FileLevelForm: 3,
+                    step4UploadProgress: 4,
+                    step5UploadSummary: 5
+                },
+
             }
         },
         computed: {
             ...mapState({
+                user: state => state.user.user,
+                upload: state => state.upload.upload,
+                createUploadInProgress: state => state.upload.createUploadInProgress,
+                newUploadCreated: state => state.upload.newUploadCreated,
             }),
         },
         watch: {
-
+            // eslint-disable-next-line no-unused-vars
+            upload: function (newVal, oldVal) {
+                if(newVal && !oldVal) {
+                    if(this.upload.upload_submission_id) {
+                        this.step = this.steps.step2FileSelection;
+                    }
+                }
+            },
+            // eslint-disable-next-line no-unused-vars
+            newUploadCreated: function (newVal, oldVal) {
+                // console.log('newUploadCreated prop changed: ', newVal, ' | was: ', oldVal);
+                if(newVal) {
+                    // console.log("new upload created");
+                    // console.log("upload id: " + this.upload._id);
+                    // console.log(`this.$route.params.id: ${this.$route.params.id}`);
+                    if(this.$route.params.id != this.upload._id ) {
+                        // console.log("re-route");
+                        this.$router.push({ name: 'upload', params: { id: this.upload._id } });
+                    }
+                }
+            },
         },
         beforeDestroy() {
+            // console.log("upload reset state");
+            this.resetState();
         },
+
     }
 </script>
 <style scoped>
