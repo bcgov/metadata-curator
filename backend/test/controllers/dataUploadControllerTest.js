8b964bdcd3542b309582d52ed3756f591b717ec0
argush3 <argus@highwaythreesolutions.com>
2020-05-21 05:50:40 -0700

    Update data upload controller to return data upload resource on post

--- backend/test/controllers/dataUploadControllerTest.js
+++ backend/test/controllers/dataUploadControllerTest.js
@@ -13,7 +13,7 @@ describe("DataUploadController", function() {
     let sandbox
     before(async () => {
         sandbox = sinon.createSandbox();
-        sandbox.stub(dataUploadService, 'createDataUpload').returns(null)
+        sandbox.stub(dataUploadService, 'createDataUpload').returns({name:"dupload"})
         sandbox.stub(dataUploadService, 'updateDataUpload').returns({name:"dupload"})
         sandbox.stub(dataUploadService, 'getDataUploadById').returns({name:"dupload"})
         sandbox.stub(dataUploadService, 'listDataUploads').returns([{name:"dupload"}])
@@ -40,7 +40,7 @@ describe("DataUploadController", function() {
         let { dataUploadController } = require('../../controllers')
         const response = resStub()
         await dataUploadController.postDataUpload({body:{name:"abc", uploader:"aa"}}, response)
-        expect(response.answer).to.equal("201 {\"message\":\"Data upload saved successfully.\"}")
+        expect(response.answer).to.equal( "201 {\"name\":\"dupload\"}");
     })
 
     it('should succeed put', async () => {
