5a2c2df9c32599ec4902233be13561da5af78370
ikethecoder <ikethecoder@canzea.com>
2020-05-21 20:22:29 -0700

    Include a diff library and tests

--- backend/test/tests.js
+++ backend/test/tests.js
@@ -3,6 +3,7 @@ process.env.NODE_ENV = 'test';
 describe("MC Unit Tests", function() {
     require('./modules/authTest');
     require('./modules/semantic_infer');
+    require('./modules/resourceDiffTest');
     require('./clients/forumClientTest');
     require('./clients/formioClientTest');
     require('./notifications/emailTest');
