3cad29e94366bfcfc52fe8d72f9fffe58a18eadf
argush3 <argus@highwaythreesolutions.com>
2020-05-29 08:11:36 -0700

    Remove topic creation logic for a dataupload when adding a comment

--- backend/services/commentService.js
+++ backend/services/commentService.js
@@ -15,16 +15,6 @@ const addComment = async (dataUploadId, user, comment) => {
         const notifyAllApprovers = alwaysNotifyUninvolvedOnCommentAdd && dataUpload.opened_by_approver
             && !dataUpload.approver_has_commented && user.isDataProvider;
 
-        //TODO: topic creation should reside in create upload endpoint implementation which hasn't currently been
-        // wired into the FE yet.  For the time being, it is included here to make it easier to test the functionality
-        // that is currently implemented
-        if(!dataUpload.topic_id) {
-            const topic = await forumClient.addTopic(dataUpload._id, user);
-
-            dataUpload = await db.DataUploadSchema.findOneAndUpdate(
-                {_id: dataUploadId},{topic_id: topic._id}, {new: true});
-        }
-
         await forumClient.addComment(dataUpload.topic_id, comment, user);
 
         if(user.isApprover) {
