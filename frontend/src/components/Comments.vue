f86808b35e3abd061ded19b32496a12557b4be44
BrandonSharratt <brandon@highwaythreesolutions.com>
2020-10-28 12:06:27 -0700

    changed header for comments to discussion and added a prompt when no comments present

--- frontend/src/components/Comments.vue
+++ frontend/src/components/Comments.vue
@@ -1,6 +1,9 @@
 <template>
     <div style="width: 85%;">
-        <v-list three-line style="margin-bottom: 15px;">
+        <div v-if="commentDisplayItems.length == 0">
+            Nothing here yet, why not start a discussion with the add comment to discussion button
+        </div>
+        <v-list v-else three-line style="margin-bottom: 15px;">
             <template v-for="(item, index) in commentDisplayItems">
                     <v-divider
                         v-if="item.divider"
