25017f642c00b1bde3a2d32f291f4033949f0f05
BrandonSharratt <brandon@highwaythreesolutions.com>
2021-06-14 13:56:40 -0700

    fixed image prepare script

--- terraform/scripts/prepare_images_variable.py
+++ terraform/scripts/prepare_images_variable.py
@@ -5,15 +5,12 @@ template = """
 
 images = {
     owner = "quay.io/h3brandon"
-    request_api = ":${TAG}"
-    policy_api = ":${TAG}"
-    project_api = ":${TAG}"
-    validate_api = ":${TAG}"
     forum_api = ":${TAG}"
-    frontend = ":${TAG}"
     formio = "h3brandon/formio:latest"
-    minio = ":RELEASE.2019-04-23T23-50-36Z"
+    frontend = ":${TAG}"
     tusd = ":68385adc0cba"
+    minio = ":RELEASE.2019-04-23T23-50-36Z"
+    frontend_bridge = ":${TAG}"    
 }
     """
 
