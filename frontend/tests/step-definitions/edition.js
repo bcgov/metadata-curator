3d825b764aebe06950682e0f533d80d44d55bdb8
BrandonSharratt <brandon@highwaythreesolutions.com>
2023-05-11 09:22:24 -0700

    more screens

--- frontend/tests/step-definitions/edition.js
+++ frontend/tests/step-definitions/edition.js
@@ -107,10 +107,13 @@ When(/^Data approver makes a new edition$/, async function(){
 
         await client.waitForElementPresent('#dataset-editions-tab', 30000);
         await client.click('#dataset-editions-tab');
-        await client.pause(10000)
+        await client.pause(10000);
+        await client.saveScreenshot('./'+path+'/preAddEdition-'+new Date().toISOString().replace(/[:.]/g, '')+'.png');
         await client.waitForElementPresent('#addVersion', 25000);
-        await client.pause(10000)
+        await client.pause(10000);
         await client.click('#addVersion');
+        await client.pause(3000);
+        await client.saveScreenshot('./'+path+'/postAddEdition-'+new Date().toISOString().replace(/[:.]/g, '')+'.png');
 
         const groupSel = helpers.confGet('providerGroup');
 
